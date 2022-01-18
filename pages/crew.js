dba2179f96221bedbf855bc39ec61eca903b31ab
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2022-01-18 11:59:08 -0600

    made some layout adjustments from mobile up

--- pages/crew.js
+++ pages/crew.js
@@ -29,16 +29,16 @@ const Crew = () => {
                 <meta name="description" content="Description for Space Tourism" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="main-wrapper h-full overflow-hidden relative bg-crew tablet:bg-crew-tablet 
+            <div className="main-wrapper h-full desktop:h-screen overflow-hidden relative bg-crew tablet:bg-crew-tablet 
             desktop:bg-crew-desktop bg-no-repeat bg-cover">
                 <Navbar />
-                <section className="my-16 crew-pg-wrapper px-8 desktop:grid desktop:grid-cols-2">
+                <section className="mt-16 crew-pg-wrapper px-8">
                     <div className=" page-title">
                         <h1 className="my-16 tracking-[3.7px] text-white text-center text-subhead1 font-barlow 
                         tracking-widest uppercase tablet:text-left tablet:pl-12"><span className="opacity-50 font-black">02</span> Meet Your Crew</h1>
                     </div>
-                    <div className="crew-container flex flex-col tablet:flex-col-reverse">
-                        <div className="crew-img-container px-[22.5%] tablet:px-[27%] desktop:px-[15%]">
+                    <div className="crew-container grid grid-rows-2 tablet:grid-rows-[200px_minmax(600px,_1fr)_0px] desktop:grid-cols-2 desktop:grid-rows-1">
+                        <div className="crew-img-container self-center tablet:self-end tablet:order-2 px-[22.5%] tablet:px-[27%] desktop:px-[15%]">
                             <Image
                                     src={data.crew[selection].images.webp}
                                     alt="Moon" 
@@ -49,10 +49,10 @@ const Crew = () => {
                                     priority
                             />
                         </div>
-                        <div className="slider-container">
+                        <div className="slider-container tablet:order-1">
                             <Tab.Group 
                             as="div"
-                            className="flex flex-col tablet:flex-col-reverse justify-center items-center"
+                            className="flex flex-col tablet:flex-col-reverse items-center desktop:items-left"
                              onChange={(index) => {
                                         handleClick(index)
                                     }}>
@@ -100,20 +100,20 @@ const Crew = () => {
                                 </Tab.List>
                                 <Tab.Panels>
                                     <Tab.Panel className="text-white p-4">
-                                        <h4 className="uppercase font-bellefair text-[2rem] text-center"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
-                                        <p className="font-barlow text-violet text-center text-[1.25rem]">{data.crew[selection].bio}</p>
+                                        <h4 className="uppercase font-bellefair text-[2rem] text-center desktop:text-left"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
+                                        <p className="font-barlow text-violet text-center text-[1.25rem] desktop:text-left">{data.crew[selection].bio}</p>
                                     </Tab.Panel>
                                     <Tab.Panel className="text-white p-4">
-                                        <h4 className="uppercase font-bellefair text-[2rem] text-center"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
-                                        <p className="font-barlow text-violet text-center text-[1.25rem]">{data.crew[selection].bio}</p>
+                                        <h4 className="uppercase font-bellefair text-[2rem] text-center desktop:text-left"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
+                                        <p className="font-barlow text-violet text-center text-[1.25rem] desktop:text-left">{data.crew[selection].bio}</p>
                                     </Tab.Panel>
                                     <Tab.Panel className="text-white p-4">
-                                        <h4 className="uppercase font-bellefair text-[2rem] text-center"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
-                                        <p className="font-barlow text-violet text-center text-[1.25rem]">{data.crew[selection].bio}</p>
+                                        <h4 className="uppercase font-bellefair text-[2rem] text-center desktop:text-left"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
+                                        <p className="font-barlow text-violet text-center text-[1.25rem] desktop:text-left">{data.crew[selection].bio}</p>
                                     </Tab.Panel>
                                     <Tab.Panel className="text-white p-4">
-                                        <h4 className="uppercase font-bellefair text-[2rem] text-center"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
-                                        <p className="font-barlow text-violet text-center text-[1.25rem]">{data.crew[selection].bio}</p>
+                                        <h4 className="uppercase font-bellefair text-[2rem] text-center desktop:text-left"><span className="text-white/50 text-[1.5rem] block">{data.crew[selection].role}</span>{data.crew[selection].name}</h4>
+                                        <p className="font-barlow text-violet text-center text-[1.25rem] desktop:text-left">{data.crew[selection].bio}</p>
                                     </Tab.Panel>
                                 </Tab.Panels>
                             </Tab.Group>
